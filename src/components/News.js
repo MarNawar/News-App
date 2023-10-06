2a5366de8af34286a161d31d77a80ca09cf99cbb
Puneet Vishnoi <puneetvishnoi2017@gmail.com>
2023-10-06 10:18:15 +0530

    finished

--- src/components/News.js
+++ src/components/News.js
@@ -17,7 +17,7 @@ function News({keepProgress, pageSize, country, catagory}) {
 
   async function update(pageNO){
     setLoading(true)
-    let url= `https://newsapi.org/v2/top-headlines?country=${country}&category=${catagory}&apiKey=${apiKey}&page=${pageNO}&pageSize=${pageSize}`;
+    let url= `https://newsapi.org/v2/top-headlines?country=${country}&category=${catagory}&apiKey='27f835bb27d147ff81f6e4486119b270'&page=${pageNO}&pageSize=${pageSize}`;
 
     let data = await fetch(url);
     let parseData = await data.json();
